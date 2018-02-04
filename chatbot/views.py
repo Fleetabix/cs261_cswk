e6aa7ea78b829a42ac628864378b65694cfebfa6
Scott Edwards <scottedwardsicoll@gmail.com>
2018-02-04 18:05:43 +0000

    added ask_chatbot page/function

--- chatbot/views.py
+++ chatbot/views.py
@@ -2,6 +2,7 @@
     This is where the views for the chatbot page are defined
 """
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -10,3 +11,10 @@ def index(request):
         Returns the html for the chatbot
     """
     return render(request, 'chatbot/index.html')
+
+def ask_chatbot(request):
+    query = request.POST.get('query')
+    data = {
+        "response": "FLORIN: you said '" + query + "'?"
+    }
+    return JsonResponse(data);
