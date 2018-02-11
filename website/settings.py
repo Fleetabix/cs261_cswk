9f3fbfa2de5bfc6a5ae4b0209c266bba369ddb3e
Scott Edwards <scottedwardsicoll@gmail.com>
2018-02-11 00:48:46 +0000

    made any emails sent print to console

--- website/settings.py
+++ website/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = '/static/'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/chatbot'
+
+# print any emails sent to the console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
