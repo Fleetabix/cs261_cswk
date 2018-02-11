76a2927a90ca392a5a1c547999bdb085b76867be
Scott Edwards <scottedwardsicoll@gmail.com>
2018-02-11 00:06:40 +0000

    added urls for account management

--- website/urls.py
+++ website/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('chatbot/', include('chatbot.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
