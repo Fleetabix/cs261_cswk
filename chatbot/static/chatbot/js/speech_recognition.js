894afca86b040e48f4079392ddcb3cadb1d8563f
Scott Edwards <scottedwardsicoll@gmail.com>
2018-02-11 20:52:51 +0000

    added callbacks to show if FLORIN is listening to your query

--- chatbot/static/chatbot/js/speech_recognition.js
+++ chatbot/static/chatbot/js/speech_recognition.js
@@ -1,11 +1,29 @@
-$(document).ready(function() {
-    if(annyang) {
+$(document).ready(function () {
+    if (annyang) {
         var command = {
-            '(ok) (hey) flooring *query': function(query) {
-                outputQuery(query);
+            '(ok) flooring *query': function (query) {
+                askChatbot(query);
             }
         }
         annyang.addCommands(command);
+        annyang.addCallback('soundstart', function () {
+            console.log("adding speech box");
+            var speechBox = "<div id='speech-box' class='message user-message'>";
+            speechBox += "<br><p>";
+            speechBox += " <i class='fas fa-circle' id='speech-dot-0'></i> ";
+            speechBox += " <i class='fas fa-circle' id='speech-dot-1'></i> ";
+            speechBox += " <i class='fas fa-circle' id='speech-dot-2'></i> ";
+            speechBox += "</p>";
+            speechBox += "</div>";
+            $("#speech-box").remove();
+            $("#messages").append(speechBox);
+            $("#messages").scrollTop($("#messages").prop("scrollHeight"));
+            animateDots();
+        });
+        annyang.addCallback('result', function () {
+            console.log("removing speech box");
+            $("#speech-box").remove();
+        });
 
         //get the user's preferences about having the microphone on
         var microOn = Cookies.get("microphone");
@@ -23,7 +41,7 @@ $(document).ready(function() {
     }
 });
 
-$("#speech-btn").click(function() {
+$("#speech-btn").click(function () {
     var microOn = Cookies.get("microphone");
     setListening(!(microOn == "true"));
     Cookies.set("microphone", (microOn == "true") ? false : true);
@@ -34,9 +52,22 @@ function setListening(on) {
     console.log("setting speech " + ((on) ? "on" : "off"));
     if (on) {
         $("#speech-btn").css("color", "red");
-        annyang.start();
+        annyang.start({ autoRestart: true, continuous: false });
     } else {
         $("#speech-btn").css("color", "var(--header-bg-colour");
         annyang.abort();
+        $("#speech-box").remove();
+    }
+}
+
+function animateDots() {
+    if (animateDots.dot == undefined) {
+        animateDots.dot = 0;
+    }
+    $("#speech-dot-" + animateDots.dot).css("color", "var(--header-bg-colour");
+    $("#speech-dot-" + ((animateDots.dot + 2) % 3)).css("color", "var(--body-fg-colour");
+    animateDots.dot = (animateDots.dot + 1) % 3;
+    if ($("#speech-box").length) {
+        setTimeout(animateDots, 500);
     }
 }
