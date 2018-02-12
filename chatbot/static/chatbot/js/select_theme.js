72aff3ff6d83c25079a0484da7cb7ad8dac94493
Scott Edwards <scottedwardsicoll@gmail.com>
2018-02-12 08:27:54 +0000

    made theme selction work with dynamic amount of themes. also added the extra properties added in last commit.

--- chatbot/static/chatbot/js/select_theme.js
+++ chatbot/static/chatbot/js/select_theme.js
@@ -1,60 +1,59 @@
-var theme1 = {
-    'name': 'Default',
-    '--header-bg-colour': "#17252A",
-    '--header-fg-colour': "#FEFFFF",
-    '--body-bg-colour': "#3AAfA9",
-    '--body-fg-colour': "#DEF2F1",
-    '--accent-colour': "#2B7A78"
-}
-
-var theme2 = {
-    'name': 'Pink',
-    '--header-bg-colour': "#FFF",
-    '--header-fg-colour': "#4A4A4A",
-    '--body-bg-colour': "#CFC5DD",
-    '--body-fg-colour': "#000",
-    '--accent-colour': "#E0DFFF",
-}
-
-var theme3 = {
-    'name': 'Light',
-    '--header-bg-colour': "#FFF",
-    '--header-fg-colour': "#000",
-    '--body-bg-colour': "#FFF",
-    '--body-fg-colour': "#000",
-    '--accent-colour': "#44DDFF",
-}
+var themes = {
+    Default: {
+        '--header-bg-colour': "#17252A",
+        '--header-fg-colour': "#FEFFFF",
+        '--body-bg-colour': "#3AAfA9",
+        '--body-fg-colour': "#DEF2F1",
+        '--accent-colour': "#2B7A78",
+        '--border-colour': "#000",
+        '--btn-colour': "#17252A"
+    },
+    Pink: {
+        '--header-bg-colour': "#FFF",
+        '--header-fg-colour': "#4A4A4A",
+        '--body-bg-colour': "#CFC5DD",
+        '--body-fg-colour': "#000",
+        '--accent-colour': "#E0DFFF",
+        '--border-colour': "#000",
+        '--btn-colour': "#FFF"
+    },
+    Light: {
+        '--header-bg-colour': "#FFF",
+        '--header-fg-colour': "#000",
+        '--body-bg-colour': "#FFF",
+        '--body-fg-colour': "#000",
+        '--accent-colour': "#44BBDD",
+        '--border-colour': "#000",
+        '--btn-colour': "#44BBDD"
+    }
+};
 
 $(document).ready(function () {
+    for (name in themes) {
+        var theme = themes[name];
+        $("#themes").append(
+            "<button id='"+name+"' class='dropdown-item theme-btn' type='button'>"
+            +name
+            +"</button>"
+        );
+    }
+
+    $(".theme-btn").on('click', function () {
+        var theme = $(this).attr("id");
+        setTheme(theme);
+    });
+
     var themeCookie = Cookies.get("theme");
-    if (themeCookie == null) {
-        Cookies.set("theme", "theme-1");
-    } else {
-        setTheme(themeCookie);
-        console.log(themeCookie);
-    }
+    console.log(themeCookie);
+    setTheme((themeCookie == null) ? "Default" : themeCookie);
 });
 
-$(".theme-btn").click(function () {
-    var theme = $(this).attr("id");
-    Cookies.set("theme", theme);
-    setTheme(theme);
-});
 
-function setTheme(theme) {
-    switch (theme) {
-        case "theme-1":
-            document.documentElement.style.setProperty('--header-bg-colour', "#17252A");
-            document.documentElement.style.setProperty('--header-fg-colour', "#FEFFFF");
-            document.documentElement.style.setProperty('--body-bg-colour', "#3AAfA9");
-            document.documentElement.style.setProperty('--body-fg-colour', "#DEF2F1");
-            document.documentElement.style.setProperty('--accent-colour', "#2B7A78");
-            break;
-        case "theme-2":
-            document.documentElement.style.setProperty('--header-bg-colour', "#FFF");
-            document.documentElement.style.setProperty('--header-fg-colour', "#4A4A4A");
-            document.documentElement.style.setProperty('--body-bg-colour', "#CFC5DD");
-            document.documentElement.style.setProperty('--body-fg-colour', "#000");
-            document.documentElement.style.setProperty('--accent-colour', "#E0DFFF");
+
+function setTheme(themeName) {
+    var theme = themes[themeName];
+    for (varName in theme) {
+        document.documentElement.style.setProperty(varName, theme[varName]);
     }
+    Cookies.set("theme", themeName);
 }
