bc61983383ec35df1bc675819f78efbccf64d447
Matthew Watson <Matthew@myself.lan>
2018-02-14 14:35:38 +0000

    Added the ability to parse data from json files.

--- chatbot/nl/dict.py
+++ chatbot/nl/dict.py
@@ -1,11 +1,21 @@
 import os
+import json
 
 def read(dictName):
-	pass
+    #Dictionaries are stored in a subdirectory, so modify the path.
+    path = os.path.join("dictionaries",dictName)
+    #Try to open the file.
+    f = openFile(path)
+    #Don't try to read a missing file.
+    if f is None:
+        return None
+    #Read the file and convert it from JSON.
+    data = f.read()
+    return json.loads(data)
 
 def openFile(fileName):
 	path=os.path.dirname(__file__)
-	path=os.path.join(path,dictName)
+	path=os.path.join(path,fileName)
 	try:
 		return open(path)
 	except IOError:
