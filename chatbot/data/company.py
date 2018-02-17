0c40ca80588d1f4e4050210123afedf6b3b1cf4d
arjunlek <arjunlekshmanan@gmail.com>
2018-02-17 14:04:17 +0000

    returns instead of printing

--- chatbot/data/company.py
+++ chatbot/data/company.py
@@ -6,10 +6,10 @@ class Company:
 		self.ticker = ticker_name
 	
 	def getSpotPrice(self):
-		print(StockHandler.getStockInformation(self.ticker).spot_price)
+		return StockHandler.getStockInformation(self.ticker).spot_price
 
 	def getSpotPriceDifference(self):
-		print(StockHandler.getStockInformation(self.ticker).price_difference)
+		return StockHandler.getStockInformation(self.ticker).price_difference
 
 	def getSpotPercentageDifference(self):
-		print(StockHandler.getStockInformation(self.ticker).percent_difference)
+		return StockHandler.getStockInformation(self.ticker).percent_difference)
