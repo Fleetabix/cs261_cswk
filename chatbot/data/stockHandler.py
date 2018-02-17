3626ef466914f4e77c03eafdb52b38d4da479fac
arjunlek <arjunlekshmanan@gmail.com>
2018-02-17 02:47:56 +0000

    added init.py file (hopefully for importing)

--- chatbot/data/stockHandler.py
+++ chatbot/data/stockHandler.py
@@ -1,9 +1,18 @@
 import feedparser
-import CompanyStock
+
+class CompanyStock:
+	def __init__(self, spotprice, change, percentchange)
+		self.spot_price = spotprice
+		self.price_difference = change
+		self.percent_difference = percentchange
+
 class StockHandler:
 
-	def __init__(self, ticker, name):
-		self.fullname = name
-		self.ticker = ticker
-	
+	def getStockInformation(ticker): 
+		# [possibly] Temporary rss feed for now - till better one is found - current implementation is slow
+		feed = feedparser.parse('https://arcane-citadel-48781.herokuapp.com/')
+		for i in range(1, len(feed)):
+			current_item = feed.entries[i].summary.split(',')
+			if (current_item[0] == ticker):
+				return CompanyStock.CompanyStock(current_item[4], current_item[5], current_item[6])
 	
