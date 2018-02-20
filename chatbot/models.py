e6077ee6a00ca82fd5a51971a2b65e21cb704936
Scott Edwards <scottedwardsicoll@gmail.com>
2018-02-20 11:14:50 +0000

    added the company model

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Company(models.Model):
+    """
+        The company model which will be stored in the database.
+        The ticker is the primary key.
+    """
+    ticker = models.CharField(primary_key=True, max_length=10)
+    name = models.CharField(max_length=40)
+    industry = models.CharField(max_length=40)
+
+    def getStockPrice(self):
+        return None
+
+    def getPercentageDifference(self):
+        return None
+
+    def getPriceOverTime(self, time):
+        return None
+
+    def getNews(self):
+        return None
+
+    def __str__(self):
+            return self.name
+
