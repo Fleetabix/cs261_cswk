7cb73f5e5ae657851d90a330dd3e5f55ff7db4c6
Scott Edwards <scottedwardsicoll@gmail.com>
2018-02-20 12:46:50 +0000

    registered the models so they can be viewed in django admin

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from chatbot.models import Company, UserQueryData, PortfolioItem
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(UserQueryData)
+admin.site.register(PortfolioItem)
