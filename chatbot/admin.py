68e48ea3159c7586f02cf11b18a3a399f256621b
Scott Edwards <scottedwardsicoll@gmail.com>
2018-02-21 11:56:42 +0000

    updated what models you can see in the admin site

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from chatbot.models import Company, UserQueryData, PortfolioItem
+from chatbot.models import Company, Trader, CompanyHitCount
 # Register your models here.
 admin.site.register(Company)
-admin.site.register(UserQueryData)
-admin.site.register(PortfolioItem)
+admin.site.register(Trader)
+admin.site.register(CompanyHitCount)
