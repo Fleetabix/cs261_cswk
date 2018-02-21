83fcec15bf7cb4831ae0132b63ea4fa40062fb0e
Matthew Watson <Matthew@myself.lan>
2018-02-21 11:14:14 +0000

    Changed the dictionary recognition to consider multiple dictionaries.

--- chatbot/nl/sentence.py
+++ chatbot/nl/sentence.py
@@ -10,29 +10,56 @@ class Sentence:
 		self.tokens = nltk.word_tokenize(sentence)
 
 	def extract(self):
-		self.qualities = self.findFromDictionary("qualities", self.tokens)
+		self.keywords = self.findFromDictionaries(["qualities", "comparatives", "connectives", "companies"], self.tokens)
 
-	def findFromDictionary(self, dictName, tokens):
-		dictionary = dict.getDict(dictName)
-		length = len(tokens) #Start with the full string.
+	def findFromDictionaries(self, dictNames, tokens):
+		#Make an empty list of dictionaries
+		dictionaries = {}
+		#Grab the corresponding dictionary and add it to the list.
+		for dictName in dictNames:
+			dictionary = dict.getDict(dictName)
+			if dictionary is not None:
+				dictionaries[dictName] = dictionary
+		#Start with the full string:
+		length = len(tokens)
 		while length>0:
 			for i in range(0,len(tokens)-length+1): #Every group of length length.
 				name = tokensToSentence(tokens[i:i+length]) #Find the potential name
-				actualName = getID(name, dictionary)
+				actualName = getIDAndDictionary(name, dictionaries)
 				if actualName != None: #If name is an alias:
-					newList = tokens[:i] + tokens [i+length:]
-					return [{"id":actualName, "typed":name}] + self.findFromDictionary(dictName, newList) #Recursion
+					#Perform this on the words before the found word
+					firstHalf = self.findFromDictionaries(dictNames, tokens[:i])
+					#And on the words after the found word
+					secondHalf = self.findFromDictionaries(dictNames, tokens[i+length:])
+					#Combine them in the correct order.
+					return firstHalf + [{"dictionary":actualName["dictionary"],"id":actualName["id"], "typed":name}] + secondHalf #Recursion
 			length-=1 #Now check shorter strings.
 		return []
 
 	def __repr__(self):
-		return "Qualities: " + self.qualities.__repr__()
+		listOfWords = ""
+		for keyword in self.keywords:
+			listOfWords+=keyword["id"]
+			listOfWords+=" "
+		#return "Keywords: " + self.keywords.__repr__() + "\n" + listOfWords
+		return listOfWords
 
 def getID(name, dictionary):
 	for entry in dictionary:
 		for alias in entry["alias"]:
-			if alias == name:
+			if wordsMatch(alias,name):
 				return entry["id"]
+
+def getIDAndDictionary(name, dictionaries):
+	for key in dictionaries:
+		dictionary = dictionaries[key]
+		for entry in dictionary:
+			for alias in entry["alias"]:
+				if wordsMatch(alias,name):
+					return {"dictionary":key, "id":entry["id"]}
+
+def wordsMatch(word1, word2):
+	return word1.lower() == word2.lower()
 
 #Takes a set of tokens and reassmbles them to make a normal sentence.
 def tokensToSentence(tokens):
