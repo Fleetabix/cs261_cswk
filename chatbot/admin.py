a5bd8469ed76b8cb78a00d092563dea2c116e9a3
Scott Edwards <scottedwardsicoll@gmail.com>
2018-02-21 15:30:47 +0000

    added the industry model to the admin site

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from chatbot.models import Company, Trader, CompanyHitCount
+from chatbot.models import Industry, Company, Trader, CompanyHitCount
 # Register your models here.
+admin.site.register(Industry)
 admin.site.register(Company)
 admin.site.register(Trader)
 admin.site.register(CompanyHitCount)
