a554e3e164ec03012ba488dfada95d6ecc341a93
arjunlek <arjunlekshmanan@gmail.com>
2018-02-23 12:32:27 +0000

    fixed stock_handler

--- chatbot/data/stock_handler.py
+++ chatbot/data/stock_handler.py
@@ -4,7 +4,7 @@ import pandas_datareader.data as web
 
 
 class CompanyStock:
-	def __init__(self, spotprice, change, percentchange):
+	def __init__(self, spotprice, change, percentchange, retrieve_datetime):
 		self.spot_price = spotprice
 		self.price_difference = change
 		self.percent_difference = percentchange
@@ -13,7 +13,7 @@ class CompanyStock:
 def getStockInformation(ticker): 
 	# [possibly] Temporary rss feed for now - till better one is found - current implementation is slow
 	f = feedparser.parse('https://arcane-citadel-48781.herokuapp.com/')
-	for stockinfo in f:
+	for stockinfo in f.entries:
 		current_item = stockinfo.summary.split(',')
 		if (current_item[0] == ticker):
 			return CompanyStock(current_item[4], current_item[5], current_item[6], f.feed.updated)
