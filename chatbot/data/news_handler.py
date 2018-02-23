825ab64687b698c738ec6fc7dd582e2f601e30be
arjunlek <arjunlekshmanan@gmail.com>
2018-02-23 19:53:58 +0000

    added image to news information

--- chatbot/data/news_handler.py
+++ chatbot/data/news_handler.py
@@ -1,15 +1,29 @@
 import feedparser
-
+import requests
+import bs4
 class NewsInformation:
-	def __init__(self, url, headline):
+	def __init__(self, url, headline, image, article_date):
 		self.url = url
-		self.keywords = []
+		self.keywords = list()
 		self.headline = headline
+		self.image = image
+		self.date_published = article_date
 
-class NewsHandler:
-	def getNews(ticker):
-		news = []
-		f = feedparser.parse('https://finance.google.com/finance/company_news?q='+ticker+'&ei=OwiKWrHYB5CWUo31grgL&output=rss')
-		for stories in f.entries:
-			news.append(NewsInformation(stories.link, stories.title))
-		return news
+
+
+def getNews(ticker):
+	news = list()
+	webpage = 'https://finance.google.com/finance/company_news?q='+ticker+'&ei=OwiKWrHYB5CWUo31grgL&output=rss'
+	f = feedparser.parse(webpage)
+	for stories in f.entries:
+		news.append(NewsInformation(stories.link, stories.title, getImage(url), ""))
+	return news
+
+def getImage(url):
+	response = requests.get(url)
+	image_url = ""
+	if (response.status_code == 200):
+		soup = bs4.BeautifulSoup(response.content, "lxml")
+		image = soup.find("meta",  property="og:image")
+		image_url = image["content"]	
+	return image_url
