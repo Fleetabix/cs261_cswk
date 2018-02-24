ea742635ae0af27ba1d11a34f08e2c5abd6cd3b1
arjunlek <arjunlekshmanan@gmail.com>
2018-02-24 15:30:04 +0000

    changed feed implementation

--- chatbot/data/stock_handler.py
+++ chatbot/data/stock_handler.py
@@ -1,7 +1,8 @@
 import feedparser
 import datetime
 import pandas_datareader.data as web
-
+import requests
+import datatime
 
 class CompanyStock:
 	def __init__(self, spotprice, change, percentchange, retrieve_datetime):
@@ -12,12 +13,15 @@ class CompanyStock:
 
 def getStockInformation(ticker): 
 	# [possibly] Temporary rss feed for now - till better one is found - current implementation is slow
-	f = feedparser.parse('https://arcane-citadel-48781.herokuapp.com/')
-	for stockinfo in f.entries:
-		current_item = stockinfo.summary.split(',')
-		if (current_item[0] == ticker):
-			return CompanyStock(current_item[4], current_item[5], current_item[6], f.feed.updated)
-
+	response = requests.get('http://m.londonstockexchange.com/exchange/mobile/stocks/summary.html?tidm='+ticker)
+	cs = None
+	if (response.status_code == 200):
+		soup = bs4.BeautifulSoup(response.content, "lxml")
+		data = soup.find("div", {"class": "tr darkEven"})
+		stock = [x.text.strip() for x in data.findAll('span')]
+		retrieved = datetime.datetime.now()-datetime.timedelta(minutes=15)
+		cs = CompanyStock(stock[0], stock[1], stock[2], retrieved)
+	return cs
 def getHistoricalStockInformation(ticker, start, end):
 	data = web.DataReader(ticker, "google", start, end)
 	return data
