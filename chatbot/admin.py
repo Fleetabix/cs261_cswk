2a6cd418fd40dbfb767cd06aba2f4acb86e1cfd4
Scott Edwards <scottedwardsicoll@gmail.com>
2018-02-25 21:02:51 +0000

    added all tables to admin site

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from chatbot.models import Industry, Company, TraderProfile, CompanyHitCount, Alias
+from chatbot.models import *
 # Register your models here.
 admin.site.register(Industry)
 admin.site.register(Company)
 admin.site.register(TraderProfile)
 admin.site.register(CompanyHitCount)
-admin.site.register(Alias)
+admin.site.register(CompanyAlias)
+admin.site.register(IndustryAlias)
