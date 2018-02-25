221ba9f59d52b07b0fab9b1538ba9a80a080fbb1
Scott Edwards <scottedwardsicoll@gmail.com>
2018-02-25 15:04:24 +0000

    added the alias model to the django admin page

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from chatbot.models import Industry, Company, TraderProfile, CompanyHitCount
+from chatbot.models import Industry, Company, TraderProfile, CompanyHitCount, Alias
 # Register your models here.
 admin.site.register(Industry)
 admin.site.register(Company)
 admin.site.register(TraderProfile)
 admin.site.register(CompanyHitCount)
+admin.site.register(Alias)
