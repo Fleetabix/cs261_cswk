c1a07021206eee0278fd81db18a3890f9e866290
arjunlek <arjunlekshmanan@gmail.com>
2018-02-26 14:17:05 +0000

    changed import on models

--- chatbot/models.py
+++ chatbot/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-import data.news_handler
-import data.stock_handler
+from data.news_handler import *
+from data.stock_handler import *
 
 # Create your models here.
 
@@ -28,38 +28,38 @@ class Company(models.Model):
     industry = models.ForeignKey(Industry, on_delete=models.CASCADE)
 
     def __str__(self):
-            return self.ticker + " - " + self.name
+        return self.ticker + " - " + self.name
     
     def getSpotPrice(self):
         """
             Returns spot price for specified company as a string
         """
-		return stock_handler.getStockInformation(self).spot_price
+		return getStockInformation(self).spot_price
 
 	def getSpotPriceDifference(self):
         """
             Returns difference between current and last spot price for specified company as a string
         """
-		return stock_handler.getStockInformation(self.ticker).price_difference
+		return getStockInformation(self.ticker).price_difference
 
 	def getSpotPercentageDifference(self):
         """
             Returns percentage difference between current and last spot price for specified company as a string
         """
-		return stock_handler.getStockInformation(self.ticker).percent_difference
+		return getStockInformation(self.ticker).percent_difference
 
 	def getStockHistory(self, start, end):
         """
             Returns a pandas DataFrame for historical prices for specified company between a start and end date,
             will include the high and low for that day and opening price
         """
-		return stock_handler.getHistoricalStockInformation(self.ticker, start, end)
+		return getHistoricalStockInformation(self.ticker, start, end)
 
 	def getNews(self):	
     """
         Returns a list of NewsInformation objects of articles related to specified company
     """	
-		return news_handler.getNews(self.ticker)
+		return getNews(self.ticker)
 
 class Alias(models.Model):
     """
