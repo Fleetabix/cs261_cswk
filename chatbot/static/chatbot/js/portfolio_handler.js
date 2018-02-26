f8761e7043b8498de88b948b4192371cb28359eb
Scott Edwards <scottedwardsicoll@gmail.com>
2018-02-26 15:18:14 +0000

    added basic ajax code

--- chatbot/static/chatbot/js/portfolio_handler.js
+++ chatbot/static/chatbot/js/portfolio_handler.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-
+    $("#searchbox").keyup(function() {
+        var query = $(this).val();
+        if (query != "") {
+            var data = {query: query};
+            $.ajax({
+                url: 'get_entities/',
+                data: data,
+                dataType: "json",
+                method: "get"
+            }).done(function (response) {
+                console.log("success!");
+                outputData(response);
+            }).fail(function (response) {
+                console.log("-----Fail-------");
+                console.log(response);
+            });
+        }
+    })
 });
