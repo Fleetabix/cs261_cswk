2d524c2e1d8ec783cc6227b3ac4df2f211abe90e
arjunlek <arjunlekshmanan@gmail.com>
2018-02-27 23:16:43 +0000

    added breaking news option

--- chatbot/data/news_handler.py
+++ chatbot/data/news_handler.py
@@ -15,7 +15,7 @@ class NewsInformation:
 """
 can be used for both industry and company
 """
-def getNews(name, keyword = None):
+def getNews(name, keyword = None, breaking = None):
 	news = list()
 	search_query = ""
 	if keyword is not None:
@@ -23,9 +23,14 @@ def getNews(name, keyword = None):
 		search_query = search_query.replace('&', ' and ').replace('/',' or ').replace(' ', '%20')
 	else:
 		search_query = name.replace(' ', '%20')
-	url = 'https://newsapi.org/v2/everything?q='+search_query+'&apiKey=d9c204a671844e58b110128b0b806c1f'
-	if (response.status_code ==  200)
-		response = requests.get(url)
+	
+	if breaking:
+		url =  'https://newsapi.org/v2/top-headlines?q='+search_query+'&apiKey=d9c204a671844e58b110128b0b806c1f'
+	else:
+		url = 'https://newsapi.org/v2/everything?q='+search_query+'&apiKey=d9c204a671844e58b110128b0b806c1f'
+    
+	response = requests.get(url)
+	if (response.status_code ==  200):
 		json_data = json.loads(response.text)
 		for stories in json_data["articles"]:
 			news.append(NewsInformation(stories["url"], stories["title"], stories["urlToImage"], stories["publishedAt"]))
