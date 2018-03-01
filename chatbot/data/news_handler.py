9c245c3e24be53688a80d2ab28be78745ab122df
arjunlek <arjunlekshmanan@gmail.com>
2018-03-01 12:17:48 +0000

    indent comment

--- chatbot/data/news_handler.py
+++ chatbot/data/news_handler.py
@@ -14,9 +14,9 @@ class NewsInformation:
 	
 
 def getNews(name, keyword = None, breaking = None):
-  """
-    can be used for both industry and company
-  """
+	"""
+		can be used for both industry and company
+	"""
 	news = list()
 	search_query = ""
 	if keyword is not None:
@@ -24,12 +24,12 @@ def getNews(name, keyword = None, breaking = None):
 		search_query = search_query.replace('&', ' and ').replace('/',' or ').replace(' ', '%20')
 	else:
 		search_query = name.replace(' ', '%20')
-	
+
 	if breaking:
 		url =  'https://newsapi.org/v2/top-headlines?q='+search_query+'&language=en&apiKey=d9c204a671844e58b110128b0b806c1f'
 	else:
 		url = 'https://newsapi.org/v2/everything?q='+search_query+'&language=en&apiKey=d9c204a671844e58b110128b0b806c1f'
-    
+
 	response = requests.get(url)
 
 	if (response.status_code ==  200):
