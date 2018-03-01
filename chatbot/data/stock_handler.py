e00014dcf4a85fc7b8b9954e841b8857705afa36
arjunlek <arjunlekshmanan@gmail.com>
2018-03-01 15:31:28 +0000

    industry stocks method removed

--- chatbot/data/stock_handler.py
+++ chatbot/data/stock_handler.py
@@ -40,24 +40,6 @@ def getStockInformation(ticker):
 	return cs
 
 
-def getIndustryStocks(tickers):
-	"""
-		Retrieves stock information from set of company tickers in a specific industry.
-		Returns a python dictionary containing names and corresponding CompanyStock object.
-	"""
-	stocks = {}
-	for ticker in tickers:
-		response = requests.get('http://m.londonstockexchange.com/exchange/mobile/stocks/summary.html?tidm='+ticker)
-		if (response.status_code == 200):
-			soup = bs4.BeautifulSoup(response.content, "lxml")
-			data = soup.find("div", {"class": "tr darkEven"})
-			stock = [x.text.strip() for x in data.findAll('span')]
-			retrieved = datetime.datetime.now()-datetime.timedelta(minutes=15)
-			stocks[ticker] = CompanyStock(stock[0], stock[1], stock[2], retrieved)
-	return stocks
-
-
-
 def getHistoricalStockInformation(ticker, start, end):
 	if datetime.datetime.now() < end:
 		raise ValueError("This date range goes into the future!")
