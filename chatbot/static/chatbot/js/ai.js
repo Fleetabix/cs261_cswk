8c6c85e50136b80be93b4899659dafe31d8aa3d1
Scott Edwards <scottedwardsicoll@gmail.com>
2018-03-03 12:43:19 +0000

    adding start of alert function

--- chatbot/static/chatbot/js/ai.js
+++ chatbot/static/chatbot/js/ai.js
@@ -1,3 +1,14 @@
 $(doument).ready(function() {
-    
+    setTimeout(function() {
+
+    }) 
 });
+
+/** Appends a message to the alert system.
+ *  Al alerts will appear on one pop-up
+ * @param {*} title 
+ * @param {*} message 
+ */
+function florinAlert(title, message) {
+
+}
