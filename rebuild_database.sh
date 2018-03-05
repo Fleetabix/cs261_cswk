89376bed79d0e14ca8bed638a39846cf13578a8c
Scott Edwards <scottedwardsicoll@gmail.com>
2018-03-05 09:01:46 +0000

    now removes migrations for you

--- rebuild_database.sh
+++ rebuild_database.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 rm db.sqlite3
+rm -r chatbot/migrations/__pycache__
+rm chatbot/migrations/$(ls chatbot/migrations/ | grep ^[0-9])
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 ./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@florin.cb', 'qwertyuiop')"
