966789b22ca62d8ca2ed3a56cb5fa812a462ee23
Scott Edwards <scottedwardsicoll@gmail.com>
2018-03-06 13:26:42 +0000

    allowed all host connecting externally

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!#aacg9jr3n*e@(hbid%=z9%7*0wi6vqqx+2n$!f1ohl1kax*r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
