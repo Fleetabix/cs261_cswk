91990a48504649765b1781c44cf5c249a02fb3ea
Scott Edwards <scottedwardsicoll@gmail.com>
2018-03-06 15:17:34 +0000

    changed python version

--- rebuild_database.sh
+++ rebuild_database.sh
@@ -2,6 +2,6 @@
 rm db.sqlite3
 rm -r chatbot/migrations/__pycache__
 rm chatbot/migrations/$(ls chatbot/migrations/ | grep ^[0-9])
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 ./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@florin.cb', 'qwertyuiop')"
+python manage.py makemigrations
+python manage.py migrate
+python ./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@florin.cb', 'qwertyuiop')"
