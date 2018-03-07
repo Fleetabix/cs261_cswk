3985187fb4d612ddc4be0ab9a73f79f8a7147c7d
Liam Fleet <liamdavid.fleet@gmail.com>
2018-03-07 10:30:19 +0000

    Added the ability to specify 3 as a variable to change python version

--- setup_project.sh
+++ setup_project.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-pip install -r requirements.txt
-python -c "import nltk; nltk.download('punkt')"
-python -c "import nltk; nltk.download('averaged_perceptron_tagger')"
-./rebuild_database.sh
-python manage.py populate_db
+
+if [[ $1 == 3 ]]; then
+	pip3 install -r requirements.txt
+	python3 -c "import nltk; nltk.download('punkt')"
+	python3 -c "import nltk; nltk.download('averaged_perceptron_tagger')"
+	./rebuild_database.sh
+	python3 manage.py populate_db
+else
+	pip install -r requirements.txt
+	python -c "import nltk; nltk.download('punkt')"
+	python -c "import nltk; nltk.download('averaged_perceptron_tagger')"
+	./rebuild_database.sh
+	python manage.py populate_db
+fi
