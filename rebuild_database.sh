dc1f1c49f9adc7d9641ad88b9b3d114f79f041b9
Scott Edwards <scottedwardsicoll@gmail.com>
2018-03-07 12:46:06 +0000

    now populates the database

--- rebuild_database.sh
+++ rebuild_database.sh
@@ -5,3 +5,4 @@ rm chatbot/migrations/$(ls chatbot/migrations/ | grep ^[0-9])
 python manage.py makemigrations
 python manage.py migrate
 python ./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@florin.cb', 'qwertyuiop')"
+python manage.py populate_db
